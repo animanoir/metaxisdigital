153f685f40b09e2e73263403a521c826a74eefcf
Óscar A. Montiel <etherealflux94@gmail.com>
2021-04-15 18:51:23 -0500

    hotfix: Adds margin-right a la licencia

--- src/components/footer.js
+++ src/components/footer.js
@@ -30,7 +30,7 @@ const Footer = () => {
             </a>
           </div>
         </div>
-        <div className='licencia'><a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/deed.es"><img alt="Creative Commons License" style={{borderWidth: '0px'}} src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /></div>
+        <div><a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/deed.es"><img className='licencia' alt="Creative Commons License" style={{borderWidth: '0px'}} src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /></div>
         <div className='footer-fantasma'><p>Una iniciativa de <a class="link-fantasma" href='https://fantasma.rip/'>fantasma</a></p></div>
       </div>
     </footer>
